f80e37dd723b3f6a391128c18421c1c3660de59b
unknown <arminjafri138386@gmail.com>
2024-09-12 01:29:46 -0700

    create instert get of database

--- view/list_employees.php
+++ view/list_employees.php
@@ -1,1 +1,17 @@
-list
+<?php 
+global $wpdb;
+$table_employees = $wpdb->prefix . 'dyme_employees';
+$result = $wpdb -> get_var(
+    "SELECT mission FROM $table_employees WHERE ID = 12"
+);
+//var_dump($result);exit;
+if ($result) {
+    echo $result . 'mission';
+}elseif($result == NULL){
+    echo 'no record found';
+}
+else{
+    echo 'no mission';;
+}
+print_r( $result );
+exit;
